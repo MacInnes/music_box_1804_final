c7ac31aaf7658218a56f08d62512507b17cc8252
MacInnes <califormula@gmail.com>
2018-08-01 09:15:03 -0600

    adds strong params to genre controller

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,8 +1,15 @@
 class Admin::GenresController < Admin::BaseController
 
   def create
-    genre = Genre.create(name: params[:genre][:name])
+    genre = Genre.create(genre_params)
     redirect_to genres_path
   end
-  
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
+
 end
