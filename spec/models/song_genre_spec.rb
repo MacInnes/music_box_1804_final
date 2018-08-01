fc01b01aeb873c6b6fb70d4291037b957eddae82
MacInnes <califormula@gmail.com>
2018-08-01 09:19:04 -0600

    adds validation tests for song-genre

--- spec/models/song_genre_spec.rb
+++ spec/models/song_genre_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe SongGenre do
+  describe 'validations' do
+    it {should validate_presence_of(:song_id)}
+    it {should validate_presence_of(:genre_id)}
+  end
   describe 'relationships' do
     it {should belong_to(:song)}
     it {should belong_to(:genre)}
